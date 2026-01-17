7c6ae6af1115ac2459ea31d182feaee42bb7c402
金子会发光 <icsjhsmqi@hotmail.com>
2026-01-18 05:53:53 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import os
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from starlette.middleware.trustedhost import TrustedHostMiddleware  
-from starlette.middleware.proxy_fix import ProxyFixMiddleware  
+from starlette.middleware.trustedhost import TrustedHostMiddleware 
+from starlette.middleware.proxy_headers import ProxyHeadersMiddleware  
 from fastapi.responses import RedirectResponse, JSONResponse
 
 # 导入我们的新生命周期管理器和路由
@@ -27,9 +27,9 @@ app = FastAPI(
     description="一个基于 Telegram 的私有文件存储系统。",
     version="2.0.0"
 )
-# 添加 ProxyFixMiddleware 来处理代理头  
+# 添加 ProxyHeadersMiddleware 来处理代理头  
 # 这使得 FastAPI 能够正确识别 HTTPS 和原始请求信息  
-app.add_middleware(ProxyFixMiddleware, num_proxies=1)  
+app.add_middleware(ProxyHeadersMiddleware)
 # 添加 TrustedHostMiddleware 来处理代理头  
 app.add_middleware(  
     TrustedHostMiddleware,  
